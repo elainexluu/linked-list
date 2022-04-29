e059379d7f0cca34c43ad33bccf5e8a16d9ffe7f
Elaine Luu <elaineluu0501@gmail.com>
2022-04-29 14:24:13 -0700

    created node class

---
+++ linkedlist.py
@@ -0,0 +1,8 @@
+# Date: April 4, 2022
+
+# create a node class
+class node():
+    # create constructor
+    def __init__(self, data=None, nextNode=None):
+        self.data = data
+        self.nextNode = nextNode
